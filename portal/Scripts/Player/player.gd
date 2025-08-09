25a51dfdd8c4e964bd7cd8b6b67e32e22816c3eb
Joseph Sary <63524400+mothecakes@users.noreply.github.com>
2025-08-08 19:34:52 -0700

    Implement basic character controller and basic portal spawning

--- portal/Scripts/Player/player.gd
+++ portal/Scripts/Player/player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
+@onready var portal_spawner = $PortalSpawn
+@onready var sprite : Sprite2D = $Sprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# public vars
+@export var speed : float = 300.0
+@export var jump_velocity : float = -400.0
+@export var portal_radius : float = 100
 
 var sprite_flipped = false;
 
-@onready var sprite : Sprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,18 +17,25 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = jump_velocity
 
 	sprite.flip_h = sprite_flipped
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		sprite_flipped = true if direction > 0 else false
-		velocity.x = move_toward(velocity.x , direction * SPEED, SPEED)
+		velocity.x = move_toward(velocity.x , direction * speed, speed)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			var portal_position = position + ((event.position - global_position).normalized() * portal_radius); 
+			portal_spawner.spawn_portal(portal_position, self.get_angle_to(event.position))
+		
